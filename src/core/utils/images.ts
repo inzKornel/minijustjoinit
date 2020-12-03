3a9ddbce15483852db3b4bc11f3b99c78e4439cd
kornel.szaltanowicz <kornel.szaltanowicz@gmail.com>
2020-12-03 13:41:24 +0100

    chore: update statically assets path

--- src/core/utils/images.ts
+++ src/core/utils/images.ts
@@ -1,9 +1,9 @@
 export const cdnImage = (name: string, placeholder: boolean, folder = '/assets', count = 10) => {
   if (process.env.NODE_ENV === 'production') {
     if (placeholder) {
-      return 'https://cdn.statically.io/img/minijustjoinit/h=' + count + '/' + folder + '/' + name;
+      return 'https://cdn.statically.io/img/inzkornel.github.io/minijustjoinit/h=' + count + '/' + folder + '/' + name;
     } else {
-      return 'https://cdn.statically.io/img/minijustjoinit/f=auto/' + folder + '/' + name;
+      return 'https://cdn.statically.io/img/inzkornel.github.io/minijustjoinit/f=auto/' + folder + '/' + name;
     }
   } else {
     return folder + '/' + name;
