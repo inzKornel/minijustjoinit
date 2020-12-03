84122158a02fbf1480e66ce3725b13e41c1c979e
kornel.szaltanowicz <kornel.szaltanowicz@gmail.com>
2020-12-03 11:27:46 +0100

    build: change assetPrefix path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/inzkornel/inzkornel.github.io/minijustjoinit/' : '',
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/inzkornel/inzkornel.github.io/gh-pages/' : '',
   async rewrites() {
     return [
       {
